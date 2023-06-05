2804c98f4f591e0178f90ee0ed79fb43c4a64e5a
DamTrongNhan <trongnhanforwork21@gmail.com>
2023-06-05 08:49:46 +0700

    update some error syntax

--- Server/src/interfaces/model/report.ts
+++ Server/src/interfaces/model/report.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export type ReportDocument = mongoose.Document & {
   village_id: mongoose.Schema.Types.ObjectId;
-  product_id: [mongoose.Schema.Types.ObjectId];
+  product_id: mongoose.Schema.Types.ObjectId[];
   code: string;
   dateReport: Date;
 };
