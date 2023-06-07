5cc8eb056b037f7d1307643d941f45f597d7b9af
NgTrHau <92238714+NgTrHau@users.noreply.github.com>
2023-06-07 08:51:47 +0700

    Them Responsive Navbar cho Header

--- Client/src/layouts/Components/Header/Header.tsx
+++ Client/src/layouts/Components/Header/Header.tsx
@@ -5,6 +5,8 @@ import config from '~/config';
 import { Link, NavLink } from 'react-router-dom';
 import Button from '~/components/Button';
 import Search from '~/components/Search';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -48,6 +50,60 @@ function Header() {
           <Button color="yellow" border="circle">
             Đăng nhập
           </Button>
+        </div>
+        <label htmlFor="nav-mobile-input" className={cx('navbars-btn')}>
+          <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+        </label>
+        <input
+          type="checkbox"
+          name=""
+          className={cx('nav-input')}
+          id="nav-mobile-input"
+        ></input>
+        <label htmlFor="nav-mobile-input" className={cx('nav-overlay')}></label>
+        <div className={cx('nav-mobile')}>
+          <label htmlFor="nav-mobile-input" className={cx('nav-mobile-close')}>
+            <FontAwesomeIcon icon={faTimes}></FontAwesomeIcon>
+          </label>
+
+          <NavLink to={config.routes.home} className={cx('logo')}>
+            CraftVillages
+          </NavLink>
+          <div className={cx('search-mobile')}>
+            <Search />
+          </div>
+          <NavLink
+            to={config.routes.villages}
+            className={(nav) => cx('navlink-mobile', { active: nav.isActive })}
+          >
+            Làng nghề
+          </NavLink>
+          <NavLink
+            to={config.routes.products}
+            className={(nav) => cx('navlink-mobile', { active: nav.isActive })}
+          >
+            Sản phẩm
+          </NavLink>
+          <NavLink
+            to={config.routes.introduce}
+            className={(nav) => cx('navlink-mobile', { active: nav.isActive })}
+          >
+            Giới thiệu
+          </NavLink>
+          <NavLink
+            to={config.routes.contact}
+            className={(nav) => cx('navlink-mobile', { active: nav.isActive })}
+          >
+            Liên hệ
+          </NavLink>
+          <div className={cx('auth-btn')}>
+            <Button color="secondary" border="circle">
+              Đăng nhập
+            </Button>
+            <Button color="white" border="circle">
+              Đăng ký
+            </Button>
+          </div>
         </div>
       </div>
     </div>
